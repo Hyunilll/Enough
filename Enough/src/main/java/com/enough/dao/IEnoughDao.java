d4f1d4ba17aec59706ddfbb60db297ccf069844a
Hyunilll <hyun1_choi@naver.com>
2023-07-31 12:29:24 +0900

    dd

--- Enough/src/main/java/com/enough/dao/IEnoughDao.java
+++ Enough/src/main/java/com/enough/dao/IEnoughDao.java
@@ -1,8 +1,18 @@
 package com.enough.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.enough.dto.ProductVO;
 
 @Mapper
 public interface IEnoughDao {
-
+	// 상품목록
+	List<ProductVO> getproductList();
+	// 추가
+	void insertpr(ProductVO pvo);
+	// 수정
+	// 삭제
+	
 }
